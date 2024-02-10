fec9f989b9cfdefc352ef6c8a9315b1d70bf3bf6
RomSmile <ryacenko3@gmail.com>
2024-02-10 20:35:52 +0200

    change location of store

--- client/lib/store-provider.tsx
+++ client/lib/store-provider.tsx
@@ -2,7 +2,7 @@
 import { useRef } from 'react';
 import { Provider } from 'react-redux';
 
-import { makeStore, AppStore } from './store';
+import { makeStore, AppStore } from '@/store';
 
 export default function StoreProvider({ children }: { children: React.ReactNode }) {
   const storeRef = useRef<AppStore>();
