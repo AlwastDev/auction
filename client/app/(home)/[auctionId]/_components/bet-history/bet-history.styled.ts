ee4c537777befdf10dbb9a3403167bf6d4112fa5
Daniil Raptanov <daniilraptanov@gmail.com>
2024-02-11 23:14:38 +0200

    FE: update of bet components

--- client/app/(home)/[auctionId]/_components/bet-history/bet-history.styled.ts
+++ client/app/(home)/[auctionId]/_components/bet-history/bet-history.styled.ts
@@ -11,3 +11,23 @@ export const Header = styled.h2`
     text-align: center;
     color: ${colors.black}
 }`;
+
+export const ContentSection = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: start;
+  border-radius: 15px;
+  width: 100%;
+  min-height: 500px;
+  background-color: ${colors.blue};
+  padding: 63px 30px 134px;
+  margin-bottom: 30px;
+  z-index: 0;
+`;
+
+export const PaginationContainer = styled.div`
+  position: absolute;
+  bottom: 44px;
+  right: 61px;
+`;
