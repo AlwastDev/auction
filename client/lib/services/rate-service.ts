3fdd8377c9c36b10d782eca2eda324c9b019429d
RomSmile <ryacenko3@gmail.com>
2024-02-11 23:30:25 +0200

    FE added rate service

---
+++ client/lib/services/rate-service.ts
@@ -0,0 +1,29 @@
+import qs from 'query-string';
+import { toast } from 'sonner';
+
+import { api } from '@/lib/services/api';
+import { Rate } from '@/lib/models';
+
+const route = 'rates';
+
+interface IGetRatesResponse {
+  rows: Rate[];
+  totalPage: number;
+}
+
+export const getRates = async (auctionId: string): Promise<IGetRatesResponse> => {
+  const url = qs.stringifyUrl(
+    {
+      url: `/${route}/${auctionId}`,
+    },
+    { skipEmptyString: true },
+  );
+
+  const response = await api.get<IGetRatesResponse>(url);
+
+  if (!response || !response.data) {
+    toast.error(response.message);
+  }
+
+  return response.data as IGetRatesResponse;
+};
